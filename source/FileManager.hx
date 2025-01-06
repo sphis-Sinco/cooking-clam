de88d17beb799982e4e11d9ff71ee16330ad616e
IdealistCat <mattallan.h@gmail.com>
2025-01-05 22:45:15 -0600

    getDataFile, getImageFile, getSoundFile, remove brackets when not needed, editor.formatOnSave = false

--- source/FileManager.hx
+++ source/FileManager.hx
@@ -5,11 +5,17 @@ class FileManager
 	public static var STATE_FOLDER:String = 'default';
 
 	public static function getAssetPath(path:String):String
-	{
 		return 'assets/$path';
-	}
+
 	public static function getAssetFile(file:String):String
-	{
 		return getAssetPath('$STATE_FOLDER/$file');
-	}
+    
+	public static function getDataFile(file:String):String
+		return getAssetFile('data/$file');
+
+	public static function getImageFile(file:String):String
+		return getAssetFile('images/$file.png');
+
+	public static function getSoundFile(file:String):String
+		return getAssetFile('$file.wav');
 }
