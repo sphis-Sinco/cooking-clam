e7efd32facec5e71e5bc257af16f27eab2f0d986
IdealistCat <mattallan.h@gmail.com>
2025-01-05 22:39:03 -0600

    getAssetFile and STATE_FOLDER

--- source/FileManager.hx
+++ source/FileManager.hx
@@ -2,8 +2,14 @@ package;
 
 class FileManager
 {
+	public static var STATE_FOLDER:String = 'default';
+
 	public static function getAssetPath(path:String):String
 	{
 		return 'assets/$path';
+	}
+	public static function getAssetFile(file:String):String
+	{
+		return getAssetPath('$STATE_FOLDER/$file');
 	}
 }
