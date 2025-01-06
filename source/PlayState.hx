c4dc02379ef1cb7f5fa4661dd92c566f057720f2
IdealistCat <mattallan.h@gmail.com>
2025-01-05 22:53:30 -0600

    Ticks, Ticks per Second Text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,41 @@
 package;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxTimer;
 
 class PlayState extends FlxState
 {
+	public var TICK:Int = 0;
+	public var TPS:Float = 0;
+
+	public var ticktext:FlxText;
+
 	override public function create()
 	{
+		ticktext = new FlxText(8,8,0,"TICK: 0\nTPS: 0", 16);
+		add(ticktext);
+
+		TPS_Timer();
+
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
+		TICK++;
+
+		ticktext.text = 'TICK: $TICK\nTPS: $TPS';
+
 		super.update(elapsed);
+	}
+
+	public function TPS_Timer()
+	{
+		var originalTick:Int = TICK;
+		FlxTimer.wait(1, () -> {
+			TPS = TICK - originalTick;
+			TPS_Timer();
+		});
 	}
 }
