4b07216e906e0f5a5baf6620dcb5106cff1c4512
IdealistCat <mattallan.h@gmail.com>
2025-01-05 22:57:01 -0600

    ticktext => DEBUG_TEXT, DEBUG_MODE, DEBUG_MODE toggle, and some performance(?) stuff

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.util.FlxTimer;
@@ -9,12 +10,14 @@ class PlayState extends FlxState
 	public var TICK:Int = 0;
 	public var TPS:Float = 0;
 
-	public var ticktext:FlxText;
+	public var DEBUG_TEXT:FlxText;
+	public var DEBUG_MODE:Bool = false;
 
 	override public function create()
 	{
-		ticktext = new FlxText(8,8,0,"TICK: 0\nTPS: 0", 16);
-		add(ticktext);
+		DEBUG_TEXT = new FlxText(8,8,0,"TICK: 0\nTPS: 0", 16);
+		DEBUG_TEXT.visible = false;
+		add(DEBUG_TEXT);
 
 		TPS_Timer();
 
@@ -25,7 +28,14 @@ class PlayState extends FlxState
 	{
 		TICK++;
 
-		ticktext.text = 'TICK: $TICK\nTPS: $TPS';
+		// should not update it if it's not visible
+		if (DEBUG_TEXT.visible) DEBUG_TEXT.text = 'TICK: $TICK\nTPS: $TPS';
+
+		// DEBUG_MODE is basically the toggle for DEBUG_TEXT's visibility
+		if (DEBUG_TEXT.visible != DEBUG_MODE) DEBUG_TEXT.visible = DEBUG_MODE;
+
+		// !DEBUG_MODE is the opposite of DEBUG_MODE so DEBUG_MODE = !DEBUG_MODE will flip the value
+		if (FlxG.keys.justReleased.F3) DEBUG_MODE = !DEBUG_MODE;
 
 		super.update(elapsed);
 	}
